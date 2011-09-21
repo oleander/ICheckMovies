58a1007b3340274bad51ac66790766eef8880116
Linus Oleander <linus@oleander.nu>
2011-09-22 00:19:04 +0200

    Implementing #name

--- lib/icheckmovies.rb
+++ lib/icheckmovies.rb
@@ -2,9 +2,11 @@ require "nokogiri"
 require "rest-client"
 
 class Icheckmovies
+  attr_reader :url
+  
   def initialize(url)
     @url = url
-    @movie = Struct.new(:imdb_link, :title)
+    @movie = Struct.new(:imdb_link, :title, :year, :details)
   end
   
   def self.fetch(url)
@@ -15,9 +17,15 @@ class Icheckmovies
     content.css("li.movie").map do |movie|
       @movie.new(
         movie.at_css("a.optionIMDB").attr("href"),
-        movie.at_css("h2 a").content
+        movie.at_css("h2 a").content,
+        movie.at_css(".year").content.to_i,
+        "http://www.icheckmovies.com" + movie.at_css("a.dvdCoverSmall").attr("href")
       )
     end
+  end
+  
+  def name
+    @_name ||= content.at_css("h1").content
   end
     
   private
--- spec/icheckmovies_spec.rb
+++ spec/icheckmovies_spec.rb
@@ -2,9 +2,11 @@ require "spec_helper"
 
 describe Icheckmovies do
   use_vcr_cassette "the+best+1000+movies+ever+made"
+  let(:href_matcher) { %r{^((http[s]?|ftp):\/)?\/?([^:\/\s]+)((\/\w+)*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?$} }
+  let(:url) { "http://www.icheckmovies.com/list/the+best+1000+movies+ever+made/" }
   
   before(:each) do
-    @check = Icheckmovies.fetch("http://www.icheckmovies.com/list/the+best+1000+movies+ever+made/")
+    @check = Icheckmovies.fetch(url)
   end
   
   it "should have 1002 movies" do
@@ -15,6 +17,16 @@ describe Icheckmovies do
     @check.movies.each do |movie|
       movie.imdb_link.should match(%r{http://www.imdb.com/title/tt\d{7}/})
       movie.title.should_not be_empty
+      movie.year.should > 1800
+      movie.details.should match(href_matcher)
     end
+  end
+  
+  it "should be able to return the given url" do
+    @check.url.should eq(url)
+  end
+  
+  it "should have a name" do
+    @check.name.should eq("The Best 1,000 Movies Ever Made top movies")
   end
 end
